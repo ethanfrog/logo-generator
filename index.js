8c3ec9b57c31b22f1f1549e8dcd1593856f930b1
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2023-12-13 16:48:19 -0500

    Added functional shape and shape color inputs

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateSVG = ({text, textColor, shape, shapeColor}) =>
-`<svg version="1.1" width="300" height="200">
+const generateSVG = ({text, textColor}, shapeData) =>
+`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-  <rect width="100%" height="100%" fill="red" />
-
-  <circle cx="150" cy="100" r="80" fill="green" />
+  ${shapeData}
 
   <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
 
@@ -39,7 +37,18 @@ inquirer
   ])
   .then((answers) => {
 
-    const logoContent = generateSVG(answers);
+    let shapeData;
+    if (answers.shape === 'Circle') {
+      shapeData = `<circle cx="150" cy="100" r="80" fill="${answers.shapeColor}" />`;
+    }
+    else if (answers.shape === 'Triangle') {
+      shapeData = `<polygon points="200,10 250,190 160,210" fill="${answers.shapeColor}" />`;
+    }
+    else {
+      shapeData = `<rect cx="150" cy="100" width="100" height="100" fill="${answers.shapeColor}" />`;
+    }
+
+    const logoContent = generateSVG(answers, shapeData);
 
     fs.writeFile(`logo.svg`, logoContent, (err) =>
       err ? console.log(err) : console.log(`Generated logo.svg`)
