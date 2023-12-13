c5ca2276981e51700d57b771d782a18332b2f5ab
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2023-12-13 15:36:42 -0500

    Added functional text and text color inputs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const generateSVG = ({text, textColor, shape, shapeColor}) =>
 
   <circle cx="150" cy="100" r="80" fill="green" />
 
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
 
 </svg>
 `;
