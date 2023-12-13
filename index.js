4cf46a209a248c1ee549d908cc01d1d38a502b81
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2023-12-13 17:36:51 -0500

    Logo text longer than 3 characters is no longer allowed

--- index.js
+++ index.js
@@ -37,20 +37,25 @@ inquirer
   ])
   .then((answers) => {
 
-    let shapeData;
-    if (answers.shape === 'Circle') {
-      shapeData = `<circle cx="150" cy="100" r="80" fill="${answers.shapeColor}" />`;
-    }
-    else if (answers.shape === 'Triangle') {
-      shapeData = `<polygon points="50,200 250,200 150,0" fill="${answers.shapeColor}" />`;
+    if (answers.text.length > 3) {
+      console.log("Logo text must be 3 characters or fewer");
     }
     else {
-      shapeData = `<rect x="75" y="25" width="150" height="150" fill="${answers.shapeColor}" />`;
+      let shapeData;
+      if (answers.shape === 'Circle') {
+        shapeData = `<circle cx="150" cy="100" r="80" fill="${answers.shapeColor}" />`;
+      }
+      else if (answers.shape === 'Triangle') {
+        shapeData = `<polygon points="50,200 250,200 150,0" fill="${answers.shapeColor}" />`;
+      }
+      else {
+        shapeData = `<rect x="75" y="25" width="150" height="150" fill="${answers.shapeColor}" />`;
+      }
+  
+      const logoContent = generateSVG(answers, shapeData);
+  
+      fs.writeFile(`logo.svg`, logoContent, (err) =>
+        err ? console.log(err) : console.log(`Generated logo.svg`)
+      );
     }
-
-    const logoContent = generateSVG(answers, shapeData);
-
-    fs.writeFile(`logo.svg`, logoContent, (err) =>
-      err ? console.log(err) : console.log(`Generated logo.svg`)
-    );
   });
